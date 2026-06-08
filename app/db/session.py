f82391dad14b60cd4b989213d93f09ab0b708b6c
CHIEDOZIE DARLINGTON EKWUEME <darlingdozy4@gmail.com>
2026-06-08 18:44:26 +0000

    fully working API

--- app/db/session.py
+++ app/db/session.py
@@ -2,21 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.core.config import settings
 
-
-DATABASE_URL = settings.DATABASE_URL
-
-
-engine = create_engine(
-    DATABASE_URL,
-    echo=True,
-)
-
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 class Base(DeclarativeBase):
     pass
-
 
 def get_db():
     db = SessionLocal()
